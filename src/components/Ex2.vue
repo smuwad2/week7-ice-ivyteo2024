b0fb99a812e506039f6759cb32b3d3e91e3dd193
ivyteo2024 <ivy.teo.2024@smu.edu.sg>
2025-10-22 03:26:04 +0000

    ex1,2,3

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,9 +3,16 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                item:"",
             }
             
+        },
+        methods:{
+            
+            add(){
+                this.items.push(this.item)
+            },
         }
     }
 </script>
@@ -14,5 +21,12 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
-   
+    <ul>
+        <li v-for="(item,index) in items">
+            {{item}}
+            <button class="btn" @click="items.splice(index,1)">Delete</button>
+        </li>
+    </ul>
+    <input v-model="item"></input>
+    <button @click="add()">Add</button>
 </template>
